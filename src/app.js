b26ec72ecbe7781a227daaffc52105c0e060c090
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2025-09-16 17:17:23 +0200

    header test complete

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import { TopNavBar } from "./components/topNav.js";
-import { HeroSection } from "./components/heroSection.js";
+import { header } from "./components/header.js";
 import { cardBox } from "./components/card.js";
 import { FooterNav } from "./components/footer.js";
 
 export function renderHomepage() {
   const main = document.createElement("main");
   main.appendChild(TopNavBar());
-  main.appendChild(HeroSection());
+  main.appendChild(header());
   main.appendChild(cardBox());
   main.appendChild(FooterNav());
   document.body.appendChild(main);
---
+++ src/components/header.js
@@ -0,0 +1,23 @@
+import headerContent from "../content/header.json";
+
+export function header() {
+  const header = document.createElement("header");
+  header.className = "w-full flex justify-center py-4";
+
+  const textContainer = document.createElement("div");
+  textContainer.className = "flex flex-col items-center";
+
+  const text1 = document.createElement("div");
+  text1.textContent = headerContent.headerTitle;
+  text1.className = "text-6xl font-bold mb-4";
+
+  const text2 = document.createElement("div");
+  text2.textContent = headerContent.headerSubtitle;
+  text2.className = "text-lg text-gray-500";
+
+  textContainer.appendChild(text1);
+  textContainer.appendChild(text2);
+  header.appendChild(textContainer);
+
+  return header;
+}
