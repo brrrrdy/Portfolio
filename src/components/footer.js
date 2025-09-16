d97c0e61a353301e8ccb8895db2f7f9cecebad71
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2025-09-16 16:58:05 +0200

    testing using json to store text content

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,18 @@
+import footerContent from "../content/footer.json";
+
 export function FooterNav() {
   const footer = document.createElement("footer");
-  footer.className = "bg-gray-800 text-white p-4 text-center";
-  footer.textContent = "Footer Navigation";
+  footer.className =
+    "w-full text-black py-4 mt-8 flex justify-between items-center px-8 text-sm";
+
+  const text1 = document.createElement("div");
+  text1.textContent = footerContent.footerText1;
+
+  const text2 = document.createElement("div");
+  text2.textContent = footerContent.footerText2;
+
+  footer.appendChild(text1);
+  footer.appendChild(text2);
+
   return footer;
 }
