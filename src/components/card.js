5429bbb696277bcd2fe484af6f87d1b0a8ef5125
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2025-09-16 17:31:56 +0200

    card test complete

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,23 @@
+import cardContent from "../content/card.json";
+
 export function cardBox() {
-  const demo = document.createElement("section");
-  demo.className = "p-8";
-  demo.textContent = "Demo Section";
-  return demo;
+  const cardBox = document.createElement("div");
+  cardBox.className = "w-full flex justify-center px-8";
+
+  const textContainer = document.createElement("div");
+  textContainer.className = "flex flex-col items-center";
+
+  const text1 = document.createElement("div");
+  text1.textContent = cardContent.cardTitle;
+  text1.className = "text-4xl font-bold mb-4";
+
+  const text2 = document.createElement("div");
+  text2.textContent = cardContent.cardText;
+  text2.className = "text-sm text-gray-500";
+
+  textContainer.appendChild(text1);
+  textContainer.appendChild(text2);
+  cardBox.appendChild(textContainer);
+
+  return cardBox;
 }
