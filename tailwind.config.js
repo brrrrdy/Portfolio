d28c2ec9100802ad45a50fdfd8255e58cf7e194a
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2025-10-08 14:24:39 +0100

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
   ],
   theme: {
     screens: {
+      
+      'mobile': '320px',
+      // => @media (min-width: 320px) { ... }
+
       'tablet': '640px',
       // => @media (min-width: 640px) { ... }
 
